5378e4e168eb5f5be508b02c59b9f62b3a708c3e
Justin Lee <smsc19970205@gmail.com>
2024-08-07 21:45:08 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,33 @@
 import streamlit as st
+from PIL import Image
+import pandas as pd
 
-st.write("Hello World")
+# Path to your local image file
+image_path = "/Users/justinlee/Desktop/AutoML Webpage/ML_webapp/python_image.png"
+
+# Open the image file
+image = Image.open(image_path)
+
+with st.sidebar:
+    st.image(image)
+    st.title("Auto ML Web Application")
+    choice = st.radio('Options', ["Upload", "Profiling", "Machine Learning", "Download"])
+    st.info("This applocation allows you to build an automated ML pipline using Streamlit, Pandas Profiling and Pycaret. And it is damn amazing!")
+
+
+if choice == "Upload":
+    st.title("Upload Your Data for Modelling!")
+    file = st.file_uploader("Upload Your Dataset Here")
+    if file:
+        df = pd.read_csv(file)
+        st.dataframe(df.head())
+        
+
+if choice == "Profiling":
+    pass
+
+if choice == "Machine Learning":
+    pass
+
+if choice == "Download":
+    pass
